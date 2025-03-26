f7423f5bd967d6945c7d0d4e04ab0f48e12bf744
duanjunwen <935724073@qq.com>
2025-03-26 19:15:06 +0800

    [feat] ops assert close

---
+++ applications/ColossalChat/tests/test_model/test_hybrid_tp_dp.py
@@ -0,0 +1,249 @@
+import json
+
+import torch
+import torch.distributed as dist
+from coati.dataset.loader import RawConversationDataset
+from coati.utils.compare_tool import open_module_tracker
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from transformers import AutoTokenizer, Qwen2ForCausalLM
+
+import colossalai
+from colossalai.accelerator import get_accelerator
+from colossalai.booster import Booster
+from colossalai.booster.plugin import HybridParallelPlugin, Plugin
+from colossalai.cluster import DistCoordinator
+from colossalai.nn.optimizer import HybridAdam
+from colossalai.testing.random import seed_all
+
+BATCH_SIZE = 1
+NUM_EPOCHS = 1
+LEARNING_RATE = 2e-5
+GRADIENT_ACCUMULATION_STEPS = 1
+DATA_PATH = "/home/duanjunwen/datasets/math_dataset_profile.jsonl"  # math_dataset_profile.jsonl math_dataset.jsonl
+MODEL_PATH = "/home/duanjunwen/models/Qwen/Qwen2.5-3B"
+Device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class RandomDataset(Dataset):
+    def __init__(self, num_samples, sequence_length, vocab_size=10000):
+        self.num_samples = num_samples
+        self.sequence_length = sequence_length
+        self.vocab_size = vocab_size
+        self.input_idx = torch.randint(0, vocab_size, (num_samples, sequence_length))
+        self.attention_mask = torch.randint(0, 2, (num_samples, sequence_length), dtype=torch.long)
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        return {"input_ids": self.input_idx[idx], "attention_mask": self.attention_mask[idx]}
+
+
+def load_model_and_tokenizer():
+    attn_impl = "eager" if get_accelerator().name == "cuda" else "flash_attention_2"
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_PATH,
+        trust_remote_code=True,
+        attn_implementation=attn_impl,
+    )
+    model = Qwen2ForCausalLM.from_pretrained(MODEL_PATH, trust_remote_code=True)
+    return tokenizer, model
+
+
+def all_reduce_mean(loss: torch.Tensor, plugin: Plugin) -> torch.Tensor:
+    loss = loss.data
+    group = getattr(plugin, "dp_group", None)
+    dist.all_reduce(loss, group=group)
+    return loss / dist.get_world_size(group)
+
+
+def test_hybrid_qwen():
+    colossalai.launch_from_torch()
+    get_accelerator()
+    coordinator = DistCoordinator()
+    tokenizer, model = load_model_and_tokenizer()
+    seed_all(42)
+    # dataset = RandomDataset(num_samples=100, sequence_length=2304)
+    dataset = RawConversationDataset(tokenizer, DATA_PATH, 1024)
+    # dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    optimizer = HybridAdam(model.parameters(), lr=LEARNING_RATE)
+    plugin = HybridParallelPlugin(
+        tp_size=2,
+        pp_size=1,
+        precision="bf16",
+        zero_stage=2,
+        cpu_offload=True,
+    )
+    # plugin = HybridParallelPlugin(tp_size=2, pp_size=2, precision="bf16", zero_stage=1, num_microbatches=4, enable_flash_attention=True)
+
+    dataloader = plugin.prepare_dataloader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        drop_last=True,
+    )
+
+    booster = Booster(plugin=plugin)
+    open_module_tracker(model)
+    model, optimizer, _, dataloader, _ = booster.boost(model, optimizer, None, dataloader)
+
+    def is_master():
+        if isinstance(plugin, HybridParallelPlugin) and plugin.pp_size > 1:
+            return coordinator.rank == coordinator.world_size - 1
+        return coordinator.is_master()
+
+    #####
+    # train
+    #####
+    model.train()
+    loss_dict = {}
+    for epoch in range(NUM_EPOCHS):
+        if booster.plugin.pp_size > 1:
+            data_iter = iter(dataloader)
+            step_bar = tqdm(
+                range(len(dataloader)),
+                desc="Step",
+                disable=not is_master(),
+            )
+            for step in step_bar:
+                print(f"data_iter {data_iter}")
+                outputs = booster.execute_pipeline(
+                    data_iter,
+                    model,
+                    criterion=lambda outputs, inputs: outputs[0],
+                    optimizer=optimizer,
+                    return_loss=True,
+                )
+                loss = outputs["loss"]
+                if booster.plugin.stage_manager.is_last_stage():
+                    global_loss = all_reduce_mean(loss, plugin)
+
+                optimizer.step()
+
+                if booster.plugin.stage_manager.is_last_stage():
+                    grad_norm = optimizer.get_grad_norm()
+                    step_bar.set_postfix({"loss": global_loss.item(), "grad_norm": grad_norm})
+
+                optimizer.step()
+                optimizer.zero_grad()
+        else:
+            total_loss = 0
+            for step, batch in enumerate(dataloader):
+                input_ids = batch["input_ids"].to(device=model.module.device)
+                attention_mask = batch["attention_mask"].to(device=model.module.device)
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids)
+                loss = outputs.loss
+                tensor_pt = {
+                    "input_ids": input_ids.to("cpu"),
+                    "attention_mask": attention_mask.to("cpu"),
+                    "logits": outputs["logits"].to("cpu"),
+                }
+                torch.save(tensor_pt, f"./tests/tensor_log/cuda/tensor_rank{dist.get_rank()}_step{step}.pt")
+                print(f"step {step} rank {dist.get_rank()} : loss {loss}")
+                loss_value = loss.detach().cpu().item()
+                loss_dict[step] = loss_value
+                loss = loss / GRADIENT_ACCUMULATION_STEPS
+                booster.backward(loss, optimizer)
+                # print(f"finish backward")
+                if (step + 1) % GRADIENT_ACCUMULATION_STEPS == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    # print(f"finish optimizer step")
+
+                total_loss += loss.item()
+            # 将字典保存为 JSON 文件
+            if dist.get_rank() == 0:
+                with open("loss_dict.json", "w") as f:
+                    json.dump(loss_dict, f)
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+
+
+def test_hybrid_qwen_fwd():
+    colossalai.launch_from_torch()
+    get_accelerator()
+    coordinator = DistCoordinator()
+    tokenizer, model = load_model_and_tokenizer()
+    seed_all(42)
+    # dataset = RandomDataset(num_samples=100, sequence_length=2304)
+    dataset = RawConversationDataset(tokenizer, DATA_PATH, 1024)
+    # dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    optimizer = HybridAdam(model.parameters(), lr=LEARNING_RATE)
+    plugin = HybridParallelPlugin(
+        tp_size=2,
+        pp_size=1,
+        precision="bf16",
+        zero_stage=2,
+        cpu_offload=True,
+    )
+    # plugin = HybridParallelPlugin(tp_size=2, pp_size=2, precision="bf16", zero_stage=1, num_microbatches=4, enable_flash_attention=True)
+
+    dataloader = plugin.prepare_dataloader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        drop_last=True,
+    )
+
+    booster = Booster(plugin=plugin)
+    open_module_tracker(model)
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.Dropout):
+            module.p = 0
+    model, optimizer, _, dataloader, _ = booster.boost(model, optimizer, None, dataloader)
+
+    def is_master():
+        if isinstance(plugin, HybridParallelPlugin) and plugin.pp_size > 1:
+            return coordinator.rank == coordinator.world_size - 1
+        return coordinator.is_master()
+
+    #####
+    # train
+    #####
+    model.eval()
+    loss_dict = {}
+    for epoch in range(NUM_EPOCHS):
+        if booster.plugin.pp_size > 1:
+            data_iter = iter(dataloader)
+            step_bar = tqdm(
+                range(len(dataloader)),
+                desc="Step",
+                disable=not is_master(),
+            )
+            for step in step_bar:
+                print(f"data_iter {data_iter}")
+                outputs = booster.execute_pipeline(
+                    data_iter,
+                    model,
+                    criterion=lambda outputs, inputs: outputs[0],
+                    optimizer=optimizer,
+                    return_loss=True,
+                )
+                loss = outputs["loss"]
+        else:
+            total_loss = 0
+            for step, batch in enumerate(dataloader):
+                input_ids = batch["input_ids"].to(device=model.module.device)
+                attention_mask = batch["attention_mask"].to(device=model.module.device)
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids)
+                loss = outputs.loss
+                tensor_pt = {
+                    "input_ids": input_ids.to("cpu"),
+                    "attention_mask": attention_mask.to("cpu"),
+                    "logits": outputs["logits"].to("cpu"),
+                }
+                torch.save(tensor_pt, f"./tests/tensor_log/cuda/tensor_rank{dist.get_rank()}_step{step}.pt")
+                print(f"step {step} rank {dist.get_rank()} : loss {loss}")
+                total_loss += loss.item()
+            # 将字典保存为 JSON 文件
+            if dist.get_rank() == 0:
+                with open("loss_dict.json", "w") as f:
+                    json.dump(loss_dict, f)
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+
+
+if __name__ == "__main__":
+    # test_hybrid_qwen()
+    test_hybrid_qwen_fwd()
---
+++ applications/ColossalChat/tests/test_model/test_rotatey_embedding.py
@@ -0,0 +1,86 @@
+import torch
+from transformers.models.qwen2.configuration_qwen2 import Qwen2Config
+from transformers.models.qwen2.modeling_qwen2 import Qwen2RotaryEmbedding
+
+from colossalai.testing.random import seed_all
+
+
+def run_qwen2_rotary_embedding():
+    seed_all(42)
+    batch_size = 2
+    seq_length = 4
+    num_heads = 2
+    head_dim = 64
+    config = Qwen2Config(
+        hidden_size=head_dim * num_heads,
+        num_attention_heads=num_heads,
+        max_position_embeddings=2048,  # 根据实际情况调整
+    )
+    input_tensor_cuda = torch.ones(batch_size, seq_length, num_heads, head_dim, device="cuda")
+    rotary_embedding_cuda = Qwen2RotaryEmbedding(config, device="cuda")
+    positions_cuda = torch.arange(seq_length, device="cuda").unsqueeze(0).expand(batch_size, -1)
+    cos_cuda, sin_cuda = rotary_embedding_cuda(input_tensor_cuda, positions_cuda)
+
+    tensor_pt = {
+        "input": input_tensor_cuda.to("cpu"),
+        "positions_cuda": positions_cuda.to("cpu"),
+        "cos_cuda": cos_cuda.to("cpu"),
+        "sin_cuda": sin_cuda.to("cpu"),
+    }
+    torch.save(tensor_pt, f"./tests/tensor_log/cuda/RotaryEmbedding.pt")
+
+
+def test_qwen2_rotary_embedding():
+    # 定义输入参数
+    batch_size = 2
+    seq_length = 4
+    num_heads = 2
+    head_dim = 64
+
+    # 创建 Qwen2Config 对象
+    config = Qwen2Config(
+        hidden_size=head_dim * num_heads,
+        num_attention_heads=num_heads,
+        max_position_embeddings=2048,  # 根据实际情况调整
+    )
+
+    # 创建输入张量
+    input_tensor = torch.randn(batch_size, seq_length, num_heads, head_dim)
+    positions = torch.arange(seq_length)
+    # 将 positions 扩展为二维张量
+    positions = positions.unsqueeze(0).expand(batch_size, -1)
+
+    # 创建 Qwen2RotaryEmbedding 实例
+    rotary_embedding = Qwen2RotaryEmbedding(config)
+
+    # 在 CPU 上运行
+    input_tensor_cpu = input_tensor.clone().cpu()
+    rotary_embedding_cpu = rotary_embedding.cpu()
+    positions_cpu = positions.cpu()
+    cos_cpu, sin_cpu = rotary_embedding_cpu(input_tensor_cpu, positions_cpu)
+
+    if torch.cuda.is_available():
+        input_tensor_cuda = input_tensor.clone().cuda()
+        rotary_embedding_cuda = rotary_embedding.cuda()
+        positions_cuda = positions.cuda()
+        cos_cuda, sin_cuda = rotary_embedding_cuda(input_tensor_cuda, positions_cuda)
+
+        max_diff = (cos_cpu - cos_cuda.cpu()).abs().max()
+        max_diff = (sin_cpu - sin_cuda.cpu()).abs().max()
+        tensor_pt = {
+            "input": input_tensor_cuda.to("cpu"),
+            "positions_cuda": positions_cuda.to("cpu"),
+            "cos_cuda": cos_cuda.to("cpu"),
+            "sin_cuda": sin_cuda.to("cpu"),
+        }
+        torch.save(tensor_pt, f"./tests/tensor_log/cuda/RotaryEmbedding.pt")
+
+        print(f"CPU 和 CUDA 输出的最大差值: {max_diff.item()}")
+        assert max_diff < 1e-5, f"精度差异过大，最大差值: {max_diff.item()}"
+    else:
+        print("CUDA 不可用，无法进行对比测试。")
+
+
+if __name__ == "__main__":
+    run_qwen2_rotary_embedding()  # fwd
+    # test_qwen2_rotary_embedding() # compare with cpu
